328c98111a116226fb9b24f15de499b495e55844
DAEfimova <daefimova@mai.education>
2025-11-01 23:38:52 +0300

    Fix pyproject.toml

--- tests/test_cd.py
+++ tests/test_cd.py
@@ -27,7 +27,7 @@ def test_cd(fs, base_path):
     result = cd(["j", "g"])
     assert "принимает ровно один аргумент" in result
 
-    result = cd(["\home"])
+    result = cd([f"{base_path}\home"])
     assert result == "SUCCESS"
 
     result = cd([".."])
